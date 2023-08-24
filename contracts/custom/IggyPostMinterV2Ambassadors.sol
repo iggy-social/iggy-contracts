dd34c664f22602c90f6849f3dc47bd72a8a30ea8
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2023-08-24 14:09:06 +0200

    iggy post minter with ambassadors

---
+++ contracts/custom/IggyPostMinterV2Ambassadors.sol
@@ -0,0 +1,272 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.17;
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { ReentrancyGuard } from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IChatTokenMinter {
+  function mint(address to, uint256 amount) external;
+}
+
+interface IIggyPostNft {
+
+  function getPostPrice (string memory _postId, address _author) external view returns (uint256);
+
+  function owner() external view returns(address);
+
+  function mint(
+    string memory _postId, 
+    address _author, 
+    address _nftReceiver, 
+    string memory _textPreview,
+    string memory _image,
+    uint256 _quantity
+  ) external returns(uint256);
+
+}
+
+interface IIggyPostEnumeration {
+  function addMintedPostId(address _user, uint256 _postId) external;
+}
+
+/**
+@title IggyPostMinterV2Ambassadors
+@notice This contract allows users to mint IggyPost NFTs and paying with ETH.
+@dev Use this contract when CHAT token is deployed.
+*/
+contract IggyPostMinterV2Ambassadors is Ownable, ReentrancyGuard {
+  address public immutable chatTokenMinterAddress;
+  address public daoAddress;
+  address public devAddress;
+  address public devFeeUpdaterAddress;
+  address public enumAddress;
+  address public immutable postAddress;
+  address public stakingAddress; // address of the staking contract
+
+  address public ambassador1;
+  address public ambassador2;
+
+  bool public enumEnabled = false;
+  bool public paused = false;
+
+  uint256 public chatEthRatio = 5; // e.g. 1_000, which means 1 ETH (or payment token) = 1,000 CHAT
+  uint256 public chatRewardsDuration = block.timestamp + (60 * 60 * 24 * 30 * 9); // CHAT rewards duration in seconds
+  uint256 public chatRewardsEnd; // timestamp when CHAT rewards end
+
+  uint256 public constant MAX_BPS = 10_000;
+  uint256 public daoFee = 0; // share of each domain purchase (in bips) that goes to the DAO/community that owns the frontend
+  uint256 public devFee = 200; // share of each domain purchase (in bips) that goes to the developer (Iggy team)
+  uint256 public ambassadorFee = 100; // fee for one ambassador (in bips)
+  uint256 public referrerFee = 200; // share of each domain purchase (in bips) that goes to the referrer
+  uint256 public stakingFee = 1600; // share of each domain purchase (in bips) that goes to the staking contract
+
+  // CONSTRUCTOR
+  constructor(
+    address _chatTokenMinterAddress,
+    address _daoAddress,
+    address _devAddress,
+    address _devFeeUpdaterAddress,
+    address _postAddress,
+    address _stakingAddress,
+    address _ambassador1,
+    address _ambassador2
+  ) {
+    require(_chatTokenMinterAddress != address(0), "IggyPostMinterV2: CHAT token cannot be zero address");
+    require(_postAddress != address(0), "IggyPostMinterV2: Post address cannot be zero address");
+
+    chatTokenMinterAddress = _chatTokenMinterAddress;
+    daoAddress = _daoAddress;
+    devAddress = _devAddress;
+    devFeeUpdaterAddress = _devFeeUpdaterAddress;
+    postAddress = _postAddress;
+    stakingAddress = _stakingAddress;
+
+    ambassador1 = _ambassador1;
+    ambassador2 = _ambassador2;
+  }
+
+  // READ
+
+  function getCurrentChatEthRatio() public view returns(uint256) {
+    // if chat rewards period ended, return 0
+    if (block.timestamp > chatRewardsEnd) {
+      return 0;
+    }
+
+    uint256 diff = chatRewardsEnd - block.timestamp;
+    uint256 diffRatio = (diff * MAX_BPS) / chatRewardsDuration;
+
+    return (chatEthRatio * diffRatio) / MAX_BPS;
+  }
+
+  // WRITE
+
+  function mint(
+    string memory _postId, 
+    address _author, 
+    address _nftReceiver, 
+    address _referrer,
+    string memory _textPreview,
+    string memory _image,
+    uint256 _quantity
+  ) external nonReentrant payable returns(uint256 tokenId) {
+    require(!paused, "Minting paused");
+
+    // find price
+    uint256 price = IIggyPostNft(postAddress).getPostPrice(_postId, _author) * _quantity;
+
+    require(msg.value >= price, "Value below price");
+
+    // send a referrer fee
+    if (referrerFee > 0 && _referrer != address(0)) {
+      uint256 referrerPayment = (price * referrerFee) / MAX_BPS;
+      (bool sentReferrerFee, ) = _referrer.call{value: referrerPayment}("");
+      require(sentReferrerFee, "Failed to send referrer fee");
+    }
+
+    // send an ambassador1 fee
+    if (ambassadorFee > 0 && ambassador1 != address(0)) {
+      uint256 ambassador1Payment = (price * ambassadorFee) / MAX_BPS;
+      (bool sentAmbassador1Fee, ) = ambassador1.call{value: ambassador1Payment}("");
+      require(sentAmbassador1Fee, "Failed to send ambassador1 fee");
+    }
+
+    // send an ambassador2 fee
+    if (ambassadorFee > 0 && ambassador2 != address(0)) {
+      uint256 ambassador2Payment = (price * ambassadorFee) / MAX_BPS;
+      (bool sentAmbassador2Fee, ) = ambassador2.call{value: ambassador2Payment}("");
+      require(sentAmbassador2Fee, "Failed to send ambassador2 fee");
+    }
+
+    // send a dev fee
+    if (devFee > 0 && devAddress != address(0)) {
+      uint256 devPayment = (price * devFee) / MAX_BPS;
+      (bool sentDevFee, ) = devAddress.call{value: devPayment}("");
+      require(sentDevFee, "Failed to send dev fee");
+    }
+
+    // send a dao fee
+    if (daoFee > 0 && daoAddress != address(0)) {
+      uint256 daoFeePayment = (price * daoFee) / MAX_BPS;
+      (bool sentDaoFee, ) = daoAddress.call{value: daoFeePayment}("");
+      require(sentDaoFee, "Failed to send dao fee");
+    }
+
+    // send a staking fee
+    if (stakingFee > 0 && stakingAddress != address(0)) {
+      uint256 stakingFeePayment = (price * stakingFee) / MAX_BPS;
+      (bool sentStakingFee, ) = stakingAddress.call{value: stakingFeePayment}("");
+      require(sentStakingFee, "Failed to send staking fee");
+    }
+
+    // send the rest to post author
+    (bool sent, ) = _author.call{value: address(this).balance}("");
+    require(sent, "Failed to send payment to the post author");
+
+    // mint the post as NFT
+    tokenId = IIggyPostNft(postAddress).mint(_postId, _author, _nftReceiver, _textPreview, _image, _quantity);
+
+    // store some stats in the enumeration contract
+    if (enumEnabled && enumAddress != address(0)) {
+      // feel free to comment out the stats that you don't need to track
+      IIggyPostEnumeration(enumAddress).addMintedPostId(_nftReceiver, tokenId);
+    }
+
+    // mint chat tokens for the NFT receiver (use only the fees to calculate the share of chat tokens, not the whole price)
+    if (chatTokenMinterAddress != address(0) && block.timestamp <= chatRewardsEnd) {
+      uint256 fees = (price * (devFee + daoFee + stakingFee + referrerFee)) / MAX_BPS;
+      IChatTokenMinter(chatTokenMinterAddress).mint(_nftReceiver, fees*getCurrentChatEthRatio());
+    }
+  }
+
+  // OWNER
+
+  // change ambassador1 address
+  function changeAmbassador1(address _ambassador1) external onlyOwner {
+    ambassador1 = _ambassador1;
+  }
+
+  // change ambassador2 address
+  function changeAmbassador2(address _ambassador2) external onlyOwner {
+    ambassador2 = _ambassador2;
+  }
+
+  // change chat token ratio
+  function changeChatEthRatio(uint256 _chatEthRatio) external onlyOwner {
+    chatEthRatio = _chatEthRatio;
+  }
+
+  /// @notice This changes the DAO address in the minter contract
+  function changeDaoAddress(address _daoAddress) external onlyOwner {
+    daoAddress = _daoAddress;
+  }
+
+  // change dao fee
+  function changeDaoFee(uint256 _daoFee) external onlyOwner {
+    require(_daoFee + devFee + referrerFee + stakingFee <= MAX_BPS, "Fees cannot be more than 100%");
+    daoFee = _daoFee;
+  }
+
+  // change enum address
+  function changeEnumAddress(address _enumAddress) external onlyOwner {
+    enumAddress = _enumAddress;
+  }
+
+  // change referrer fee
+  function changeReferrerFee(uint256 _referrerFee) external onlyOwner {
+    require(daoFee + devFee + _referrerFee + stakingFee <= MAX_BPS, "Fees cannot be more than 100%");
+    referrerFee = _referrerFee;
+  }
+
+  function changeStakingAddress(address _stakingAddress) external onlyOwner {
+    stakingAddress = _stakingAddress;
+  }
+
+  // change staking fee
+  function changeStakingFee(uint256 _stakingFee) external onlyOwner {
+    require(daoFee + devFee + referrerFee + _stakingFee <= MAX_BPS, "Fees cannot be more than 100%");
+    stakingFee = _stakingFee;
+  }
+
+  /// @notice Recover any ERC-20 token mistakenly sent to this contract address
+  function recoverERC20(address tokenAddress_, uint256 tokenAmount_, address recipient_) external onlyOwner {
+    IERC20(tokenAddress_).transfer(recipient_, tokenAmount_);
+  }
+
+  function toggleEnumEnabled() external onlyOwner {
+    enumEnabled = !enumEnabled;
+  }
+
+  function togglePaused() external onlyOwner {
+    paused = !paused;
+  }
+
+  /// @notice Withdraw native coins from contract
+  function withdraw() external onlyOwner {
+    (bool success, ) = owner().call{value: address(this).balance}("");
+    require(success, "Failed to withdraw native coins from contract");
+  }
+
+  // OTHER WRITE METHODS
+
+  /// @notice This changes the developer's address in the minter contract
+  function changeDevAddress(address _devAddress) external {
+    require(msg.sender == devAddress, "Sender is not the developer");
+    devAddress = _devAddress;
+  }
+
+  /// @notice This changes the dev fee updater's address in the minter contract
+  function changeDevFeeUpdaterAddress(address _devFeeUpdaterAddress) external {
+    require(msg.sender == devFeeUpdaterAddress, "Sender is not the dev fee updater");
+    devFeeUpdaterAddress = _devFeeUpdaterAddress;
+  }
+
+  // change dev fee (only dev fee updater can change it)
+  function changeDevFee(uint256 _devFee) external {
+    require(msg.sender == devFeeUpdaterAddress, "Sender is not the dev fee updater");
+    require(daoFee + _devFee + referrerFee + stakingFee <= MAX_BPS, "Fees cannot be more than 100%");
+    devFee = _devFee;
+  }
+ 
+}
---
+++ scripts/custom/minterV2ambassadors.deploy.js
@@ -0,0 +1,78 @@
+// Deploy minter V2 contract
+// npx hardhat run scripts/custom/minterV2ambassadors.deploy.js --network songbird
+// It will automatically set different fees (if needed) and set the staking contract address (if needed).
+// It will also automatically add the minter to the ChatTokenMinter contract and change the minter address in the post contract.
+// If any of these actions fail, you must do them manually.
+
+const contractName = "IggyPostMinterV2Ambassadors";
+
+const chatTokenMinterAddress = "0x31CfDF366dd9753b8443B6fc3c59598415697131";
+const daoAddress = "0x6771F33Cfd8C6FC0A1766331f715f5d2E1d4E0e2"; // DAO or web3 community which owns the frontend
+const devAddress = "0x6771F33Cfd8C6FC0A1766331f715f5d2E1d4E0e2"; // person or entity that is doing the development
+const devFeeUpdaterAddress = "0x6771F33Cfd8C6FC0A1766331f715f5d2E1d4E0e2"; // the address that can change dev fee (can be a multisig)
+const postAddress = "0xE33F27496A9cE75313f6d1FA2BA95657Fc904387"; // post NFT contract
+const stakingContractAddress = "0xCA9749778327CD67700d3a777731a712330beB9A"; // if you don't have it yet, leave it blank (but you'll need to set it later)
+const ambassador1 = "0x17a2063e1f5C6034F4c94cfb0F4970483647a2E5"; // ambassador 1 address
+const ambassador2 = "0x772bA1Faf2a2b49B452A5b287B2165cba89EfAE2"; // ambassador 2 address
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  // deploy contract
+  const contract = await ethers.getContractFactory(contractName);
+  const instance = await contract.deploy(
+    chatTokenMinterAddress,
+    daoAddress,
+    devAddress,
+    devFeeUpdaterAddress,
+    postAddress,
+    stakingContractAddress,
+    ambassador1,
+    ambassador2
+  );
+
+  console.log(contractName + " contract address:", instance.address);
+
+  await instance.deployed();
+
+  console.log("Deploy transaction mined!");
+
+  console.log("Add post minter contract address as a minter in the ChatTokenMinter contract");
+
+  // add as minter to ChatTokenMinter contract
+  const chatTokenMinterContract = await ethers.getContractFactory("ChatTokenMinter");
+  const chatTokenMinterInstance = await chatTokenMinterContract.attach(chatTokenMinterAddress);
+  const tx1 = await chatTokenMinterInstance.addMinter(instance.address);
+  await tx1.wait();
+
+  // change minter address in post contract
+  console.log("Change minter address in post contract");
+  console.log("MAKE SURE THE POST CONTRACT IS OWNED BY THIS DEPLOYER! (Otherwise make the transaction manually)");
+
+  const postContract = await ethers.getContractFactory("IggyPostNft1155");
+  const postContractInstance = await postContract.attach(postAddress);
+
+  const postContractOwner = await postContractInstance.owner();
+  console.log("Post contract owner:", postContractOwner);
+
+  if (String(postContractOwner).toLowerCase() == String(deployer.address).toLowerCase()) {
+    const tx2 = await postContractInstance.ownerChangeMinterAddress(instance.address);
+    await tx2.wait();
+  } else {
+    console.log("Post contract is not owned by this deployer. Please change the minter address manually.");
+  }
+  
+  // verify contract
+  console.log("Wait a minute and then run this command to verify contract on block explorer:");
+  console.log("npx hardhat verify --network " + network.name + " " + instance.address + " " + chatTokenMinterAddress + " " + daoAddress + " " + devAddress + " " + devFeeUpdaterAddress + " " + postAddress + " " + stakingContractAddress + " " + ambassador1 + " " + ambassador2);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
